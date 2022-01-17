3318844002f81bc31f6047106d6c18272646e258
Gautam-8 <91519436+Gautam-8@users.noreply.github.com>
2022-01-17 19:15:10 +0530

    Update PrivateRoutes.jsx

--- c4/src/components/PrivateRoutes.jsx
+++ c4/src/components/PrivateRoutes.jsx
@@ -3,11 +3,14 @@ import { Navigate } from "react-router-dom";
 
 export const PrivateRoute = ({children}) => {
 
+    console.log(children)
+
     const toggle = useSelector((state) => state.jobState.toggle);
     const auth = useSelector((state) => state.jobState.auth);
 
 if(!auth){
     return <Navigate to={'/'}/>
 }
+
 return children
 }
